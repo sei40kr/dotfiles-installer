e34251bce5c61a758e7101e01172e026e112214e
Seong Yong-ju <sei40kr@gmail.com>
2019-06-20 11:19:53 +0900

    feat: add editorconfig installer

---
+++ modules/+devtools/editorconfig.pl
@@ -0,0 +1,12 @@
+# editorconfig.pl --- EditorConfig installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+if (&is_macos) {
+    brew_install('editorconfig');
+}
+
+1;
