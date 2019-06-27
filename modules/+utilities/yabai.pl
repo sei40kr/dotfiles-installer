e274542534d5794b9d0574f56fe554f6cdd32ee1
Seong Yong-ju <sei40kr@gmail.com>
2019-06-27 23:58:29 +0900

    feat: replace chunkwm with yabai
    Replace chunkwm with yabai, because chunkwm is archived now and will be no
    longer maintained.

--- modules/+utilities/yabai.pl
+++ modules/+utilities/yabai.pl
@@ -1,4 +1,4 @@
-# chunkwm.pl --- chunkwm
+# yabai.pl --- yabai - A tiling window manager for macOS based on binary space partitioning
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 use utf8;
@@ -11,23 +11,23 @@ use Install::PathResolver;
 if (&is_macos) {
     brew_tap('koekeishiya/formulae');
 
-    # install chunkwm
-    brew_install('chunkwm');
-    ln( dotfile('chunkwm/chunkwmrc'), "${ENV{HOME}}/.chunkwmrc" );
-    brew_services_start('chunkwm');
+    # install yabai
+    brew_install('yabai');
+    ln( dotfile('yabai/yabairc'), "${ENV{HOME}}/.yabairc" );
+    brew_services_start('yabai');
 
     # install skhd
     brew_install('skhd');
-    ln( dotfile('chunkwm/skhdrc'), "${ENV{HOME}}/.skhdrc" );
+    ln( dotfile('yabai/skhdrc'), "${ENV{HOME}}/.skhdrc" );
     brew_services_start('skhd');
 
     # install ubersicht and the widgets
     brew_cask_install('ubersicht');
     ln( dotfile('ubersicht/widgets/bar-on-top.widget'),
-"${ENV{HOME}}/Library/Application Support/Übersicht/widgets/bar-on-top.widget"
+        "${ENV{HOME}}/Library/Application Support/Übersicht/widgets/bar-on-top.widget"
     );
     ln( dotfile('ubersicht/widgets/bar-on-bottom.widget'),
-"${ENV{HOME}}/Library/Application Support/Übersicht/widgets/bar-on-bottom.widget"
+        "${ENV{HOME}}/Library/Application Support/Übersicht/widgets/bar-on-bottom.widget"
     );
 }
 
