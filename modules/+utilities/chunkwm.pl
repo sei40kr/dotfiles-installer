c676a8c172e1ff9409e310a733e9fb66062fba30
Seong Yong-ju <sei40kr@gmail.com>
2019-06-28 09:08:18 +0900

    feat: restore chunkwm installer
    Restore chunkwm installer since yabai couldn't replace chunkwm yet.

---
+++ modules/+utilities/chunkwm.pl
@@ -0,0 +1,34 @@
+# chunkwm.pl --- chunkwm
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+use FindBin;
+use lib "${FindBin::Bin}/lib";
+use Install::PathResolver;
+
+if (&is_macos) {
+    brew_tap('koekeishiya/formulae');
+
+    # install chunkwm
+    brew_install('chunkwm');
+    ln( dotfile('chunkwm/chunkwmrc'), "${ENV{HOME}}/.chunkwmrc" );
+    brew_services_start('chunkwm');
+
+    # install skhd
+    brew_install('skhd');
+    ln( dotfile('chunkwm/skhdrc'), "${ENV{HOME}}/.skhdrc" );
+    brew_services_start('skhd');
+
+    # install ubersicht and the widgets
+    brew_cask_install('ubersicht');
+    ln( dotfile('ubersicht/widgets/bar-on-top.widget'),
+"${ENV{HOME}}/Library/Application Support/Übersicht/widgets/bar-on-top.widget"
+    );
+    ln( dotfile('ubersicht/widgets/bar-on-bottom.widget'),
+"${ENV{HOME}}/Library/Application Support/Übersicht/widgets/bar-on-bottom.widget"
+    );
+}
+
+1;
