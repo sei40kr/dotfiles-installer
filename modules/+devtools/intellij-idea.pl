da00de26e997ca98f6729a11f8b3f65080e98a70
Seong Yong-ju <sei40kr@gmail.com>
2019-06-28 10:18:13 +0900

    feat: install intelli-space in intelij-idea installer

--- modules/+devtools/intellij-idea.pl
+++ modules/+devtools/intellij-idea.pl
@@ -11,6 +11,10 @@ use Install::PathResolver;
 if (&is_macos) {
     brew_cask_install('intellij-idea');
 
+    # install intelli-space, Spacemacs like key bindings for Intellij Platform
+    git_clone( 'https://github.com/MarcoIeni/intelli-space.git',
+        "${ENV{HOME}}/intelli-space" );
+
     ln( dotfile('intellij-idea/ideavimrc'), "${ENV{HOME}}/.ideavimrc" );
 }
 
